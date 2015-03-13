208686c02a05888747506e983365cc13d1d7b91a
AngeloKai <hliao16@cmc.edu>
2015-03-13 16:03:33 -0700

    added parse query adapter

--- app/src/main/java/com/example/ruoxilu/criticalmass/ListActivity.java
+++ app/src/main/java/com/example/ruoxilu/criticalmass/ListActivity.java
@@ -6,16 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.parse.ParseObject;
+import com.parse.ParseQueryAdapter;
+
 /**
  * Created by tingyu on 2/23/15.
  */
 public class ListActivity extends Activity {
 
+    private ParseQueryAdapter<ParseObject> eventListAdapter;
     LinearLayout mActivityOne;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        eventListAdapter = new ParseQueryAdapter<ParseObject>(this, "MassEvent");
+        // TODO Depend on whether we assign name to events.
+        // eventListAdapter.setTextKey("name");
+        // TODO Disable this until new icons are created
+        // eventListAdapter.setImageKey("icons");
+
+        // Disable paignation.
+        eventListAdapter.setPaginationEnabled(false);
+
 
         mActivityOne = (LinearLayout)findViewById(R.id.activity_1);
         mActivityOne.setOnClickListener(new View.OnClickListener() {
